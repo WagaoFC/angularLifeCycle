ca470c9608c45dc37944d263499daf7a74fd01b9
WagaoFC <wagnerferreira_07@hotmail.com>
2023-06-09 22:16:21 -0300

    :sparkles: feat: deleting list items

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, OnDestroy } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/iItem';
 
@@ -7,10 +7,11 @@ import { Item } from 'src/app/interfaces/iItem';
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit, OnChanges {
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
 
   @Input() item!: Item
   @Output() emitindoItemParaEditar = new EventEmitter()
+  @Output() emitindoIdParaDeletar = new EventEmitter()
 
   faPen = faPen;
   faTrash = faTrash
@@ -23,5 +24,14 @@ export class ItemComponent implements OnInit, OnChanges {
 
   editarItem() {
     this.emitindoItemParaEditar.emit(this.item)
+  }
+
+  deletarItem() {
+    console.log('deletar')
+    this.emitindoIdParaDeletar.emit(this.item)
+  }
+
+  ngOnDestroy() {
+    console.log('OnDestroy')
   }
 }
